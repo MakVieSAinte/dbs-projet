0042096088364e4a2130b960fc04320228462036
MakVieSAinte <makviesainte@gmail.com>
2024-10-15 14:00:41 +0100

    my first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./**/*.html",   
+    "./**/*.js",     
+    "./**/*.css",  
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// ==== 925DEV ====
